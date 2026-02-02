ba0a80ed99488cecfafccc7580d833dae5c2bd0c
CRaph0705 <raphael.cochrane94@gmail.com>
2026-02-02 14:45:03 +0100

    ex00 added

--- ex00/includes/easyfind.hpp
+++ ex00/includes/easyfind.hpp
@@ -1,56 +1,19 @@
 #pragma once
-
 #include <iostream>
 #include <stdexcept>
-// retval ??
+
 template <typename T>
-int easyfind(T container, int i)
+bool easyfind(T &container, int i)
 {
-	for (T<int>::iterator it = container.std::begin(); it != container.std::end(); it++)
+	typename T::const_iterator it;
+	for (it = container.begin(); it != container.end(); ++it)
 	{
 		if (*it == i)
-			return (it);
+		{
+			std::cout<< "Value " << i << " found." << std::endl;
+			return (true);
+		}
 	}
-	throw std::invalid_argument("Value not found.");
-	// pseudo code
-	// if (! std::find i in container)
-		// throw ;
-	// else 
-		// return un truc
-		// iterator ??
+	std::cout<< "Value " << i << " not found." << std::endl;
+	return (false);
 }
-
-
-/* 
-std::vector<int> vec = {1, 2, 3, 4};
-for (auto it = std::begin(vec); it != std::end(vec); ++it) {
-    std::cout << *it << " ";
-}   
-*/
-
-
-/* 
-int main()
-{
-    // Use C++14 brace-init syntax to invoke BigObject(string).
-    // The s suffix invokes string ctor. It is a C++14 user-defined
-    // literal defined in <string>
-    BigObject b1{ "42F"s };
-    BigObject b2{ "52F"s };
-    BigObject b3{ "62F"s };
-    set<BigObject, less<>> myNewSet; // C++14
-    myNewSet.insert(b1);
-    myNewSet.insert(b2);
-    myNewSet.insert(b3);
-    auto it = myNewSet.find(string("62F"));
-    if (it != myNewSet.end())
-        cout << "myNewSet element = " << it->id << endl;
-    else
-        cout << "element not found " << endl;
-
-    // Keep console open in debug mode:
-    cout << endl << "Press Enter to exit.";
-    string s;
-    getline(cin, s);
-    return 0;
-} */
--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
 #include "easyfind.hpp"
 #include <vector>
+#include <list>
+#include <stdlib.h>
 
 
-int main()
+int main(int ac, char **av)
 {
-	std::cout << "plop" << std::endl;
+	int i;
+	if (ac > 2)
+	{
+		std::cout << "Please enter a value to search " << std::endl;
+		return (1);
+	}
+	if ( ac == 1)
+		i = 42;
+	else
+		i = std::atoi(av[1]);
+	// std::cout << "plop" << std::endl;
 	std::vector<int> vec;
 	vec.push_back(42);
 	vec.push_back(12);
@@ -14,6 +26,18 @@ int main()
 	vec.push_back(-18);
 	vec.push_back(4242);
 	vec.push_back(22);
-	std::cout << easyfind(vec, 22 )<< std::endl;
+
+	easyfind(vec, i);
+
+	std::list<int> l;
+	l.push_back(42);
+	l.push_back(43);
+	l.push_back(44);
+	l.push_back(45);
+	l.push_back(46);
+	l.push_back(47);
+	l.push_back(48);
+
+	easyfind(l, i);
 	return (0);
 }
